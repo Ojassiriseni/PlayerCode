092e891b676c2a5c9e94f8711ec571f72fc1344f
Ojassiriseni <147253556+Ojassiriseni@users.noreply.github.com>
2023-10-07 07:50:51 -0500

    Add files via upload

---
+++ LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    int currentScene;
+
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+    }
+
+  
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(currentScene + 1);
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(currentScene);
+    }
+
+    public void QuitGame()
+    {
+        //Application.Quit(); for pc builds only
+        Debug.Log(" You Quit");
+    }
+}
